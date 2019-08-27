c203b110d6a72022da7fb19589b6ea6004a3d0f9
drodrigues21 <ddvilar21@hotmail.com>
2019-08-27 16:07:22 +0900

    basic styles

--- index.php
+++ index.php
@@ -5,15 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina|Cinzel+Decorative|Comfortaa|Forum|Megrim|Montserrat|Mountains+of+Christmas|Oleo+Script|PT+Mono|Pompiere|Ubuntu+Mono&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Brush+Script|Nanum+Gothic+Coding|Poor+Story|Single+Day&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="styles/main.css">
     <title>Melon4Foreigners</title>
 </head>
 
 <body>
     <div id="main_container">
-        <section id="header">Hello David's Branch</section>
-        <section id="playlist"></section>
+       <section id="header">
+            <div class="logo"><img src="images/watermelon_04.png" alt=""></div>
+            <div class="genre_container">
+                <div class="genre">Top 100</div>
+                <div class="genre">K-Pop</div>
+                <div class="genre">Rap/Hiphop</div>
+                <div class="genre">R&B</div>
+                <div class="genre">Indie</div>
+                <div class="genre">Rock</div>
+                <div class="genre">Balad</div>
+                <div class="genre">Trot</div>
+                <div class="genre">Blues/Folk</div>
+            </div>
+        </section>
+        <section id="playlist_container">
+            <div id="playlist">
+                <div class="rank"></div>
+                <div class="rank_img"></div>
+                <div class="rank01"></div>
+                <div class="rank02"></div>
+                <div class="rank03"></div>
+            </div>
+        </section>
     </div>
-    <script src="./scripts/melon.js"></script>
+    <script src="scripts/melon.js"></script>
 </body>
 
 </html>
--- scripts/melon.js
+++ scripts/melon.js
@@ -1,12 +1,63 @@
 function getSomething(xhr) {
+
+    // Get playlist from Melon
     let response = xhr.responseText;
     let div = document.createElement('div');
     div.innerHTML = response;
-    let a = div.querySelector('.rank01>span>a');
-    console.log(a);
+
+    let melon_rank = div.querySelectorAll('#lst50 .rank');
+    let melon_rank_img = div.querySelectorAll('.image_typeAll>img');
+    let melon_rank01 = div.querySelectorAll('.rank01>span>a');
+    let melon_rank02 = div.querySelectorAll('.rank02>span>a');
+    let melon_rank03 = div.querySelectorAll('.rank03>a');
+
+
+    for (var i = 0; i < 100; i++) {
+
+        var rank = document.querySelectorAll('.rank');
+        rank.textContent = melon_rank[i];
+        // var rank_img = melon_rank_img[i];
+        // console.log(test.textContent);
+        // console.log(test2);
+    }
+
+
+
+    // let melon_rank = div.querySelector('#lst50 .rank').textContent;
+    // let melon_rank_img = div.querySelector('.image_typeAll>img');
+    // let melon_rank01 = div.querySelector('.rank01>span>a').textContent;
+    // let melon_rank02 = div.querySelector('.rank02>span>a').textContent;
+    // let melon_rank03 = div.querySelector('.rank03>a').textContent;
+    // console.log(melon_rank);
+    // console.log(melon_rank_img);
+    // console.log(melon_rank01);
+    // console.log(melon_rank02);
+    // console.log(melon_rank03);
+
+
+    // Display playlist on watermelon
+
+    // let rank = document.querySelector('.rank');
+    // rank.textContent = melon_rank;
+    // let rank_img = document.querySelector('.rank_img');
+    // rank_img.appendChild(melon_rank_img);
+    // let rank01 = document.querySelector('.rank01');
+    // rank01.textContent = melon_rank01;
+    // let rank02 = document.querySelector('.rank02');
+    // rank02.textContent = melon_rank02;
+    // let rank03 = document.querySelector('.rank03');
+    // rank03.textContent = melon_rank03;
+
+
+    // console.log(rank02);
 
     // div.appendChild(a);
-    // document.body.appendChild(a);
+    // container.appendChild(a);
+    // container.appendChild(b);
+    // container.appendChild(c);
+    // container.appendChild(d);
+    // container.appendChild(e);
+
 }
 
 function getPlaylist(param) {
@@ -16,7 +67,7 @@ function getPlaylist(param) {
     xhr.onreadystatechange = function () {
         // console.log(xhr);
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            console.log(xhr.responseText);
+            // console.log(xhr.responseText);
             getSomething(xhr);
         }
     }
